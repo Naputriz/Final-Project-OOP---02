c66a0b65d101aed628a0a48d4c9d21a63a455505
RanaAqilaK <rana.aqilak@gmail.com>
2025-12-20 13:50:56 +0700

    Refactored Character Selection into CharacterFactory from GameScreen

---
+++ Frontend/core/src/main/java/com/kelompok2/frontend/factories/CharacterFactory.java
@@ -0,0 +1,41 @@
+package com.kelompok2.frontend.factories;
+
+import com.kelompok2.frontend.entities.*;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CharacterFactory {
+    @FunctionalInterface
+    private interface CharacterCreator {
+        GameCharacter create(float x, float y);
+    }
+
+    private static final Map<String, CharacterCreator> characterRegistry = new HashMap<>();
+
+    static {
+        characterRegistry.put("Ryze", Ryze::new);
+        characterRegistry.put("Isolde", Isolde::new);
+        characterRegistry.put("Insania", Insania::new);
+        characterRegistry.put("Blaze", Blaze::new);
+        characterRegistry.put("Whisperwind", Whisperwind::new);
+        characterRegistry.put("Aelita", Aelita::new);
+        characterRegistry.put("Aegis", Aegis::new);
+        characterRegistry.put("Lumi", Lumi::new);
+    }
+
+    public static GameCharacter createCharacter(String characterId, float x, float y){
+        if (characterId == null){
+            System.err.println("[CharacterFactory] Character ID is null, defaulting to Isolde.");
+            return new Isolde(x, y);
+        }
+
+        CharacterCreator creator = characterRegistry.get(characterId);
+
+        if (creator != null){
+            return creator.create(x,y);
+        } else {
+            System.err.println("[CharacterFactory] Character ID undefined: " + characterId + ", defaulting to Isolde.");
+            return new Isolde(x, y);
+        }
+    }
+}
--- Frontend/core/src/main/java/com/kelompok2/frontend/screens/GameScreen.java
+++ Frontend/core/src/main/java/com/kelompok2/frontend/screens/GameScreen.java
@@ -18,6 +18,7 @@ import com.kelompok2.frontend.entities.Insania;
 import com.kelompok2.frontend.entities.Blaze;
 import com.kelompok2.frontend.entities.Whisperwind;
 import com.kelompok2.frontend.entities.Aelita;
+import com.kelompok2.frontend.factories.CharacterFactory;
 import com.kelompok2.frontend.utils.InputHandler;
 import com.kelompok2.frontend.managers.AssetManager;
 import com.kelompok2.frontend.managers.GameManager;
@@ -65,37 +66,8 @@ public class GameScreen extends ScreenAdapter {
         // Initialize pools
         projectilePool = new ProjectilePool(50);
 
-        // Spawn Player based on character selection
-        switch (selectedCharacter) {
-            case "Ryze":
-                player = new Ryze(0, 0);
-                break;
-            case "Isolde":
-                player = new Isolde(0, 0);
-                break;
-            case "Insania":
-                player = new Insania(0, 0);
-                break;
-            case "Blaze":
-                player = new Blaze(0, 0);
-                break;
-            case "Whisperwind":
-                player = new Whisperwind(0, 0);
-                break;
-            case "Aelita":
-                player = new Aelita(0, 0);
-                break;
-            case "Aegis":
-                player = new com.kelompok2.frontend.entities.Aegis(0, 0);
-                break;
-            case "Lumi":
-                player = new com.kelompok2.frontend.entities.Lumi(0, 0); // No EnemyPool yet
-                break;
-            default:
-                player = new Isolde(0, 0);
-                this.selectedCharacter = "Isolde";
-                break;
-        }
+        // Call factory to create player char
+        player = CharacterFactory.createCharacter(selectedCharacter, 0, 0);
 
         // Initialize EnemyPool
         enemyPool = new EnemyPool(player, 30);
